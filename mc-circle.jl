cd4a5376cca2857be446656afca5b00b4449d229
Matthew Heffernan <mrhheffernan@gmail.com>
2025-01-31 21:01:04 -0800

    Switch to using imports for readability, docstrings, add types, add variable radius

--- mc-circle.jl
+++ mc-circle.jl
@@ -1,23 +1,42 @@
 # Math's hello world: Calculating pi via Monte Carlo
-using Plots
-using ArgParse
+import Plots: plot, plot!, xlabel!, ylabel!, savefig
+using ArgParse: ArgParseSettings, @add_arg_table, parse_args
 
-radius = 1
+function check(sample_x::Float64, sample_y::Float64, radius::Float32)::Bool
+    """
+    Check if a sample is inside a given radius
 
-function check(sample_x, sample_y)
+    Args:
+        sample_x: x coordinate
+        sample_y: y coordinate
+        radius: radius of the circle to check
+
+    Returns:
+        Whether or not the sample is inside the radius
+    """
     # Check if sample is in the circle
     sample_radius = (sample_x ^ 2 + sample_y ^ 2)^0.5
     return sample_radius < radius
 end
 
-function integrate_quadrant(n_samples)
+function integrate_quadrant(n_samples::Int, radius::Float32)::Tuple{Float64, Matrix{Float64}, Matrix{Float64}, Matrix{Float64}}
+    """
+    Integrates a quadrant of a circle to calculate pi
+
+    Args:
+        n_samples: number of samples
+        radius: radius of the circle to consider
+
+    Returns:
+        success_ratio, samples, samples inside the circle, samples outside the circle
+    """
     success = 0    
-    samples = rand(n_samples, 2) # 2D integration only
+    samples = radius * rand(n_samples, 2) # 2D integration only
     samples_inside = []
     samples_outside = []
 
     for i in 1:n_samples
-        checker = check(samples[i,1], samples[i,2])
+        checker = check(samples[i,1], samples[i,2], radius)
         if checker
             success = success + 1
             push!(samples_inside, samples[i,:])
@@ -37,6 +56,11 @@ function parse_commandline()
             arg_type = Int
             default = 100
             required = true
+        "--radius", "-r"
+            help = "Radius of circle, used as a demo"
+            arg_type = Float32
+            default = 1.0
+            required = false
         "--plot", "-p"
             help = "Output a plot"
             action = :store_true
@@ -49,13 +73,15 @@ function main()
     # Allow user to specify number of samples
     args = parse_commandline()
     n_samples = args["num_samples"]
+    r = args["radius"]
 
-    quadrant, samples, samples_inside, samples_outside = integrate_quadrant(n_samples)
+    quadrant, samples, samples_inside, samples_outside = integrate_quadrant(n_samples, r)
     pi_estimate = 4 * quadrant
 
     println("pi: $pi_estimate")
 
     if args["plot"]
+        # Only plot if flag is passed
         p = plot(samples_inside[:,1], samples_inside[:,2], seriestype=:scatter, label="Samples: Inside")
         plot!(samples_outside[:,1], samples_outside[:,2], seriestype=:scatter, label="Samples: Outside")
 
