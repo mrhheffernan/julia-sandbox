2f78a3b3d67e98600c35bd7b5d23054031146530
Matthew Heffernan <mrhheffernan@gmail.com>
2025-02-01 13:48:01 -0800

    Format with JuliaFormatter

--- mc-circle.jl
+++ mc-circle.jl
@@ -15,11 +15,14 @@ function check(sample_x::Float64, sample_y::Float64, radius::Float32)::Bool
         Whether or not the sample is inside the radius
     """
     # Check if sample is in the circle
-    sample_radius = (sample_x ^ 2 + sample_y ^ 2)^0.5
+    sample_radius = (sample_x^2 + sample_y^2)^0.5
     return sample_radius < radius
 end
 
-function integrate_quadrant(n_samples::Int, radius::Float32)::Tuple{Float64, Matrix{Float64}, Matrix{Float64}, Matrix{Float64}}
+function integrate_quadrant(
+    n_samples::Int,
+    radius::Float32,
+)::Tuple{Float64,Matrix{Float64},Matrix{Float64},Matrix{Float64}}
     """
     Integrates a quadrant of a circle to calculate pi
 
@@ -30,21 +33,24 @@ function integrate_quadrant(n_samples::Int, radius::Float32)::Tuple{Float64, Mat
     Returns:
         success_ratio, samples, samples inside the circle, samples outside the circle
     """
-    success = 0    
+    success = 0
     samples = radius * rand(n_samples, 2) # 2D integration only
     samples_inside = []
     samples_outside = []
 
-    for i in 1:n_samples
-        checker = check(samples[i,1], samples[i,2], radius)
+    for i = 1:n_samples
+        checker = check(samples[i, 1], samples[i, 2], radius)
         if checker
             success = success + 1
-            push!(samples_inside, samples[i,:])
+            push!(samples_inside, samples[i, :])
         else
-            push!(samples_outside, samples[i,:])
+            push!(samples_outside, samples[i, :])
         end
     end
-    return success / n_samples, samples, reduce(vcat, transpose.(samples_inside)), reduce(vcat, transpose.(samples_outside))
+    return success / n_samples,
+    samples,
+    reduce(vcat, transpose.(samples_inside)),
+    reduce(vcat, transpose.(samples_outside))
 end
 
 function parse_commandline()
@@ -52,18 +58,18 @@ function parse_commandline()
 
     @add_arg_table s begin
         "--num_samples", "-n"
-            help = "Number of samples"
-            arg_type = Int
-            default = 100
-            required = true
+        help = "Number of samples"
+        arg_type = Int
+        default = 100
+        required = true
         "--radius", "-r"
-            help = "Radius of circle, used as a demo"
-            arg_type = Float32
-            default = 1.0
-            required = false
+        help = "Radius of circle, used as a demo"
+        arg_type = Float32
+        default = 1.0
+        required = false
         "--plot", "-p"
-            help = "Output a plot"
-            action = :store_true
+        help = "Output a plot"
+        action = :store_true
     end
 
     return parse_args(s)
@@ -82,8 +88,20 @@ function main()
 
     if args["plot"]
         # Only plot if flag is passed
-        p = plot(samples_inside[:,1], samples_inside[:,2], seriestype=:scatter, label="Samples: Inside", xlabel="X Coordinate", ylabel="Y Coordinate")
-        plot!(samples_outside[:,1], samples_outside[:,2], seriestype=:scatter, label="Samples: Outside")
+        p = plot(
+            samples_inside[:, 1],
+            samples_inside[:, 2],
+            seriestype = :scatter,
+            label = "Samples: Inside",
+            xlabel = "X Coordinate",
+            ylabel = "Y Coordinate",
+        )
+        plot!(
+            samples_outside[:, 1],
+            samples_outside[:, 2],
+            seriestype = :scatter,
+            label = "Samples: Outside",
+        )
 
         savefig(p, "plot.png")
     end
